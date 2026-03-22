0f626e5f2cfba127007fede61d882cb1330d444e
tem <t@t.com>
2026-03-22 21:16:09 +0800

    qemu conf changed

--- flake.nix
+++ flake.nix
@@ -19,19 +19,42 @@
   };
 
   outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./hosts/virtualbox/configuration.nix # 指向机器特定配置
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.dhzp = import ./hosts/virtualbox/home.nix;
-        }
-      ];
+    # 使用 nixosConfigurations 定义多台机器
+    nixosConfigurations = {
+      # 机器 1: 之前的 VirtualBox 配置
+      # 建议这里的名字与 hosts/virtualbox/configuration.nix 里的 networking.hostName 一致
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/virtualbox/configuration.nix # 指向 VirtualBox 的配置
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.dhzp = import ./hosts/virtualbox/home.nix;
+          }
+        ];
+      };
+      # 机器 2: 现在的 QEMU 配置
+      # 名字对应 hosts/qemu/configuration.nix 里的 networking.hostName = "nixos-qemu"
+      nixos-qemu = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/qemu/configuration.nix # 指向 QEMU 的配置
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.dhzp = import ./hosts/qemu/home.nix;
+          }
+        ];
+      };
+
+      # 如果以后有新机器（比如笔记本），直接在这里按样板添加即可
     };
   };
 }
